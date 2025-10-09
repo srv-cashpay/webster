6623bae439ef1197342de04591132e067f8c3253
Asep rayana <asep.rayana09@gmail.com>
2025-10-09 16:57:58 +0700

    remove google

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="PASTE_CLIENT_ID_KAMU">
       <App />
-    </GoogleOAuthProvider>
   </React.StrictMode>
 );
