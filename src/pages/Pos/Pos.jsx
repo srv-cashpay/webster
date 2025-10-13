69c7248678f60d19e423e5edaf13669dade77cdf
Asep rayana <asep.rayana09@gmail.com>
2025-10-13 16:55:43 +0700

    new api

--- src/pages/Pos/Pos.jsx
+++ src/pages/Pos/Pos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./POS.css";
+import "./Pos.css";
 import Headbar from "./Headbar";
 import { getProducts } from "../../services/pos/get_product";
 
