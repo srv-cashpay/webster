ad0c7813026e473643562f4fe1924f746cbb6cb1
Asep rayana <asep.rayana09@gmail.com>
2025-12-20 18:59:28 +0700

    gsc

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaYoutube, FaLinkedin } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer({ language }) {
@@ -27,26 +27,36 @@ export default function Footer({ language }) {
           target="_blank"
           rel="noopener noreferrer"
           className="footer-icon"
+          aria-label="Instagram Cashpay"
         >
           <FaInstagram size={26} />
         </a>
-
         <a
           href="https://www.facebook.com/share/1AqYgAzggh/"
           target="_blank"
           rel="noopener noreferrer"
           className="footer-icon"
+           aria-label="Facebook Cashpay"
         >
           <FaFacebook size={26} />
         </a>
-
         <a
           href="https://www.youtube.com/"
           target="_blank"
           rel="noopener noreferrer"
           className="footer-icon"
+          aria-label="YouTube Cashpay"
         >
           <FaYoutube size={26} />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/cashpay-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-icon"
+          aria-label="LinkedIn Cashpay"
+        >
+          <FaLinkedin size={26} />
         </a>
       </div>
     </footer>
