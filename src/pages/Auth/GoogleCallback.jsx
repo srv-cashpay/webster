4ea133698405b097d5eb44110ab516147009fb4f
Asep rayana <asep.rayana09@gmail.com>
2026-01-21 12:47:32 +0700

    auth

--- src/pages/Auth/GoogleCallback.jsx
+++ src/pages/Auth/GoogleCallback.jsx
@@ -11,28 +11,41 @@ const GoogleCallback = () => {
   const langPrefix = isEnglish ? "/en" : "";
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
+    const handleGoogleCallback = async () => {
+      try {
+        const params = new URLSearchParams(window.location.search);
+        const code = params.get("code");
 
-    if (!code) {
-      navigate(`${langPrefix}/login`, { replace: true });
-      return;
-    }
+        if (!code) {
+          navigate(`${langPrefix}/login`, { replace: true });
+          return;
+        }
 
-    axios
-      .post("https://api.cashpay.co.id/auth/web/google", { code })
-      .then((res) => {
-        const data = res.data.data;
+        const res = await axios.post(
+          "https://api.cashpay.co.id/auth/web/google",
+          { code }
+        );
+
+        const data = res.data?.data;
+
+        if (!data?.token) {
+          throw new Error("Token tidak ditemukan");
+        }
 
         Cookies.set("token", data.token);
         Cookies.set("refresh_token", data.refresh_token);
         localStorage.setItem("token", data.merchant_id || data.token);
 
-        navigate(`https://console.cashpay.co.id/${langPrefix}/harbour`, { replace: true });
-      })
-      .catch(() => {
+        window.location.replace(
+          `https://console.cashpay.co.id${langPrefix}/harbour`
+        );
+      } catch (error) {
+        console.error("Google OAuth failed:", error);
         navigate(`${langPrefix}/login`, { replace: true });
-      });
+      }
+    };
+
+    handleGoogleCallback();
   }, [navigate, langPrefix]);
 
   return <p>Signing in with Google...</p>;
